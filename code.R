3f21a0cd2d3e61da8668b3194baa471009758a6d
chrissycoakley <christina.coakley@gov.scot>
2025-01-21 10:31:25 +0000

    commit to branch

--- code.R
+++ code.R
@@ -1,27 +1,3 @@
-# Some test R code
-x <- 1
-y <- 2
 
-
-# Test calculation
-
-calc.test<-x+y
-
-# View calculation
-calc.test
-
-
-# Extra change
-- what happens now?
-  
-# Attempting a change here to see if I can do some changes in r
-
-  
-  
-# Now adding change after I have sent a pull request to Euan 20 Jan 24
-=======
-
-# Resolving conflict by including both text below (from main) and code above (from chrissy branch)
-HELLO EVERYBODY
 
 
